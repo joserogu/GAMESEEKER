6f178d9f05b317f7b702418d39a7a466ea88b283
joserogu <josemanuel.rodriguez.guerrero.alu@iesfernandoaguilar.es>
2022-11-11 17:21:24 +0100

    Arreglo nombres de clientes

--- gameseek/gameseek/urls.py
+++ gameseek/gameseek/urls.py
@@ -32,33 +32,33 @@ urlpatterns = [
     
     path("clients/", ClientListView.as_view(), name="clients-list"),
     path("clients/<int:pk>/", ClientDetailView.as_view(), name="clients-detail"),
-    path("register/", ClientCreateView.as_view(), name='client-add'),
-    path('clients/<int:pk>/edit/', ClientUpdateView.as_view(), name='client-update'),
-    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client-delete'),
+    path("register/", ClientCreateView.as_view(), name='clients-add'),
+    path('clients/<int:pk>/edit/', ClientUpdateView.as_view(), name='clients-update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='clients-delete'),
 
     #EVENTOS
 
     path('eventos/', EventListView.as_view(), name="events-list"),
     path('eventos/<int:pk>/', EventDetailView.as_view(), name='events-detail'),
-    path("eventos/add/", EventCreateView.as_view(), name='event-add'),
-    path('eventos/<int:pk>/edit/', EventUpdateView.as_view(), name='event-update'),
-    path('eventos/<int:pk>/delete/', EventDeleteView.as_view(), name='event-delete'),
+    path("eventos/add/", EventCreateView.as_view(), name='events-add'),
+    path('eventos/<int:pk>/edit/', EventUpdateView.as_view(), name='events-update'),
+    path('eventos/<int:pk>/delete/', EventDeleteView.as_view(), name='events-delete'),
 
     #COMUNIDADES
 
     path('comunidades/', CommunityListView.as_view(), name="communitys-list"),
     path('comunidades/<int:pk>/', CommunityDetailView.as_view(), name='communitys-detail'),
-    path("comunidades/add/", CommunityCreateView.as_view(), name='community-add'),
-    path('comunidades/<int:pk>/edit/', CommunityUpdateView.as_view(), name='community-update'),
-    path('comunidades/<int:pk>/delete/', CommunityDeleteView.as_view(), name='community-delete'),
+    path("comunidades/add/", CommunityCreateView.as_view(), name='communitys-add'),
+    path('comunidades/<int:pk>/edit/', CommunityUpdateView.as_view(), name='communitys-update'),
+    path('comunidades/<int:pk>/delete/', CommunityDeleteView.as_view(), name='communitys-delete'),
 
     #JUEGOS
 
     path('juegos/', GameListView.as_view(), name="games-list"),
     path('juegos/<int:pk>/', GameDetailView.as_view(), name='games-detail'),
-    path("juegos/add/", GameCreateView.as_view(), name='game-add'),
-    path('juegos/<int:pk>/edit/', GameUpdateView.as_view(), name='game-update'),
-    path('juegos/<int:pk>/delete/', GameDeleteView.as_view(), name='game-delete'),
+    path("juegos/add/", GameCreateView.as_view(), name='games-add'),
+    path('juegos/<int:pk>/edit/', GameUpdateView.as_view(), name='games-update'),
+    path('juegos/<int:pk>/delete/', GameDeleteView.as_view(), name='games-delete'),
 
     #API
     path('api/', include(api.urls)),
