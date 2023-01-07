8c87d9663ac957e71e2ce6f491087dfd267b586a
root <root@ip-172-31-87-226.ec2.internal>
2023-01-07 23:43:28 +0000

    Eventos lleva a comunidad

--- gameseek/gameseek_app/views.py
+++ gameseek/gameseek_app/views.py
@@ -105,6 +105,11 @@ class EventListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['comunidades']=Event.objects.get(pk=self.request.user.pk)
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
 
